ba98dc33b08485d21ebb3fa76d198bf4e2d74ea6
i.kin <kin.1991@inbox.ru>
2023-03-16 16:31:32 +0300

    lesson1

---
+++ lesson1/task5/task5.py
@@ -0,0 +1,10 @@
+proceeds = int(input("Введите значение выручки:"))
+costs = int(input("Введите значение издержек:"))
+if proceeds > costs:
+    print(f"Ваша прибыль составляет: {proceeds-costs}")
+    rent = (proceeds-costs)/proceeds
+    print(f"Рентабельность составила: {rent}")
+    count_workers = int(input("Введите кол-во сотрудников:"))
+    print(f"Прибыль фирмы на одного сотрудника составляет:{(proceeds-costs)/count_workers}")
+else:
+    print(f"Ваш убыток составляет:{costs-proceeds}")
